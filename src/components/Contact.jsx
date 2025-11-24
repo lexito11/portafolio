2b091c582616aa0c179ce6953480a23abb75c82c
lexito11 <159784768+lexito11@users.noreply.github.com>
2025-11-23 19:28:08 -0500

    Actualizar enlaces de redes sociales y estilos de hover: actualizar enlaces de Facebook, Instagram, TikTok y GitHub con URLs reales; cambiar hovers de amarillo a blanco con letras negras en tecnologÃ­as y botones de proyectos

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
   const socialLinks = [
     { icon: 'fa-brands fa-facebook', label: 'Facebook', url: 'https://www.facebook.com/share/16Cs6H6v4m/' },
     { icon: 'fa-brands fa-instagram', label: 'Instagram', url: 'https://www.instagram.com/alex.perea11?igsh=bmk4Z2NjNGU5N21h' },
-    { icon: 'fa-brands fa-tiktok', label: 'TikTok', url: 'https://www.tiktok.com/@alex_perea11?_r=1&_t=ZS-91eUxRLtBnG' }
+    { icon: 'fa-brands fa-tiktok', label: 'TikTok', url: 'https://www.tiktok.com/@alex_perea11?_r=1&_t=ZS-91eV1te6ySW' }
   ]
 
   const handleEmailClick = () => {
